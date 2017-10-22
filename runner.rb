348abc3476cce3c5c4e6c64049e6611fdde7b4a2
Robert Richardson <rich.richardson@gamut.com>
2017-10-22 13:57:31 -0500

    Adds comments

--- runner.rb
+++ runner.rb
@@ -3,10 +3,12 @@ require_relative './emoji_decode'
 
 def run()
   puts "Enter your secret message"
-  input = gets.chomp
+  input = gets.chomp # Waits for input from user
+
   output = emoji_encode(input)
   # output = emoji_decode(input)
   puts output;
 end
 
+# //// Runner area ////
 run
